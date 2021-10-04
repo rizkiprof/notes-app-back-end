a726d8148f0985cc7c587fecdf2579856db7442d
Rizki <rizkiproff@gmail.com>
2021-10-05 05:30:35 +0700

    add production and development mode

--- src/server.js
+++ src/server.js
@@ -1,11 +1,10 @@
-/* eslint-disable linebreak-style */
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
